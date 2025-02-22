f2d6c3641a08765d7f55faf8c5023b908aae8be1
Estimaj <joao.pedro.estima@hotmail.com>
2025-02-22 16:16:41 +0000

    Google Analytics tag

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -28,4 +28,15 @@
     }">
         {{ $slot }}
     </body>
+
+    @if(app()->environment('production'))
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-THF9G6T66L"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-THF9G6T66L');
+        </script>
+    @endif
 </html>
